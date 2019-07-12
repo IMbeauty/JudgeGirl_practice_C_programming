ae1e92156203cb70d9e587f57d5669197b8a1466
IMbeauty <b07705053@ntu.edu.tw>
2019-07-12 12:54:54 +0800

    Update 203.cpp (AC)

--- 203.cpp
+++ 203.cpp
@@ -16,36 +16,36 @@ int main(){
 		y2 += dy2;
 		//if touch the wall
 		if (x1 >= X) {
-			//x1 = 2*X - x1;
+			x1 = 2*X - x1;
 			dx1 = -dx1;
-		} else if (x1 <= 0) {
-			//x1 = -x1;
+		} else if (x1 <= 1) {
+			x1 = 2 - x1;
 			dx1 = -dx1;
 		}
 		if (x2 >= X) {
-			//x2 = 2*X - x2;
+			x2 = 2*X - x2;
 			dx2 = -dx2;
-		} else if (x2 <= 0) {
-			//x2 = -x2;
+		} else if (x2 <= 1) {
+			x2 = 2 - x2;
 			dx2 = -dx2;
 		}
 		if (y1 >= Y) {
-			//y1 = 2*Y - y1;
+			y1 = 2*Y - y1;
 			dy1 = -dy1;
-		} else if (y1 <= 0) {
-			//y1 = -y1;
+		} else if (y1 <= 1) {
+			y1 = 2 - y1;
 			dy1 = -dy1;
 		}
 		if (y2 >= Y) {
-			//y2 = 2*Y - y2;
+			y2 = 2*Y - y2;
 			dy2 = -dy2;
-		} else if (y2 <= 0) {
-			//y2 = -y2;
+		} else if (y2 <= 1) {
+			y2 = 2 - y2;
 			dy2 = -dy2;
 		}
 		//collidation
 		if (x1 == x2 && y1 == y2){
-			printf("collide!\n");
+			//printf("collide!\n");
 			if(dx1 != dx2){
 				dx1 = -dx1;
 				dx2 = -dx2;
@@ -56,7 +56,7 @@ int main(){
 			}
 		}
 		
-		printf ("%d %d %d %d\n", x1, y1, x2, y2);
+		//printf ("%d %d %d %d\n", x1, y1, x2, y2);
 		//printf ("	change is %d %d %d %d\n", dx1, dy1, dx2, dy2);
 	}
 	
